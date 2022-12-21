410c4db3b0d8695b35a13e6a0d642c94e6bc6cea
Mensch <Mensch@abakus.local>
2022-12-21 23:43:26 +0100

    logToDb.py: Add debug with local logfile instead of fileinput

--- Logger/logToDb.py
+++ Logger/logToDb.py
@@ -2,10 +2,19 @@
 
 from LogParser import LogParser
 import fileinput
+debug = True
 
 if __name__ == "__main__":
     parser = LogParser()
-    with fileinput.input() as f_input:
-        for line in f_input:
+    if debug:
+        logfileLocation = "./logexmp.log"
+        with open(logfileLocation) as file:
+            content = file.readlines()
+        for line in content:
             parser.parseLog(line)
-            print(parser.getData())  # change to write in DB
+            print(parser.getData())
+    else:
+        with fileinput.input() as f_input:
+            for line in f_input:
+                parser.parseLog(line)
+                print(parser.getData())  # change to write in DB
