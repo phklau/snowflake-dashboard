8f7690e84147288ba6d3b8f98f3335e7ed6995d2
Mensch <Mensch@abakus.local>
2022-12-22 20:33:11 +0100

    LogParser.py: Refactor write data direct into dict

--- Logger/LogParser.py
+++ Logger/LogParser.py
@@ -1,58 +1,44 @@
 import re
 
+
 class LogParser:
     __m_data = {}
-    dataLogged = False
+    __defaultData = {'Date': "",
+                     'Time': "",
+                     'Error': int(True),
+                     'Errortype': "Sythax",
+                     'ErrorDescription': "Doesn`t go overwritten",
+                     'Connections': 0,
+                     'Upload': 0,
+                     'Download': 0}
 
-    def getData(self):
-        if not self.dataLogged:
-            # Writeinto Error? But whole error handling is happening in parseLog?
-            print(self.dataLogged)
-        return self.__m_data
-    def parseLog(self, logline):
-        date = ""
-        time = ""
-        connections = 0
-        upload = 0
-        download = 0
-        error = True
-        errorType = "Sythax"
-        errorDiscription = "Doesn't got overwritten"
+    def toDict(self, logline):
+        if self.__parseLog(logline):
+            return self.__m_data
+        else:
+            return None
 
-        self.__m_data = {}
-
-        date = re.search(r'[0-9]+\/[0-9]+\/[0-9]+', logline).group()
-        time = re.search(r'\d{2}:\d{2}:\d{2}', logline).group()
+    def __parseLog(self, logline):
+        self.__m_data = self.__defaultData.copy()
+        self.__m_data['Date'] = re.search(r'[0-9]+\/[0-9]+\/[0-9]+', logline).group()
+        self.__m_data['Time'] = re.search(r'\d{2}:\d{2}:\d{2}', logline).group()
         if re.search("ERROR", logline):
-           error = True
-           try:
-               errorType = re.search(r'\w+(?= ERROR)', logline).group()
-               errorDiscription = re.search(r'(?<=\d{2}:\d{2}:\d{2} )[^\)]+', logline).group()
-           except AttributeError:
-               errorType = "Parser"
-               errorDiscription = logline
+            self.__m_data['Error'] = int(True)
+            try:
+                self.__m_data['Errortype'] = re.search(r'\w+(?= ERROR)', logline).group()
+                self.__m_data['ErrorDescription'] = re.search(r'(?<=\d{2}:\d{2}:\d{2} )[^\)]+', logline).group()
+            except AttributeError:
+                self.__m_data['Errortype'] = "Parser"
+                self.__m_data['ErrorDescription'] = logline
         else:
             try:
-                connections = re.search(r'\d+(?= connections.)', logline).group()
-                upload = re.search(r'\d+(?= MB,)', logline).group()
-                download = re.search(r'\d+(?= MB.)', logline).group()
-                error = False
-                errorType = ""
-                errorDiscription = ""
+                self.__m_data['Connections'] = int(re.search(r'\d+(?= connections.)', logline).group())
+                self.__m_data['Upload'] = int(re.search(r'\d+(?= MB,)', logline).group())
+                self.__m_data['Download'] = int(re.search(r'\d+(?= MB.)', logline).group())
+                self.__m_data['Error'] = int(False)
+                self.__m_data['Errortype'] = ""
+                self.__m_data['ErrorDescription'] = ""
             except AttributeError:
-                error = True
-                errorType = "Parser"
-                errorDiscription = logline
-            self.__m_data.update({"Connections": connections,
-                                "Upload": upload,
-                                "Download": download,
-                                  })
-        # append date time error
-        self.__m_data.update({
-                            "Date": date,
-                            "Time": time,
-                            "Error": error,
-                            "Errortype": errorType,
-                            "Error discription": errorDiscription
-                              })
-        self.dataLogged = bool(self.__m_data != {})
+                self.__m_data['Errortype'] = "Parser"
+                self.__m_data['ErrorDescription'] = logline
+        return bool(self.__m_data != self.__defaultData)
