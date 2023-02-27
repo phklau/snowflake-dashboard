cebfeedd1ff7033cd06ddcbe2410a95698038aaa
Mensch <Mensch@abakus.local>
2023-02-27 21:01:02 +0100

    SQliteDB.py: Add parameter functionality for querries

--- Dashboard/SqliteDB.py
+++ Dashboard/SqliteDB.py
@@ -2,7 +2,7 @@ import sqlite3
 
 
 class SqliteDB:
-    def __init__(self, pathtodb):
+    def __init__(self, pathtodb: str):
         try:
             self.__connection = sqlite3.connect(pathtodb)
             self.__connected = True
@@ -12,17 +12,21 @@ class SqliteDB:
     def __del__(self):
         self.__connection.close()
 
-    def query_data(self, statement):
+    def is_db_connected(self) -> bool:
+        return self.__connected
+
+    def query_data(self, statement, params: tuple = ()):
         """
         Querry data in the connected db
         :param statement: String with the statement
+        :param params: Parameters of the querry
         :return: fetched data of sql query
         """
         if self.__connected:
             with self.__connection:
                 c = self.__connection.cursor()
                 try:
-                    c.execute(statement)
+                    c.execute(statement, params)
                     data = c.fetchall()
                     c.close()
                     return data
