6e6ed5ea82185aa811b81fe2a34e60d50ddbf057
phklau <philipp.klausen@web.de>
2023-07-27 19:51:27 +0200

    main.py: Build app layout from external file

--- Dashboard/main.py
+++ Dashboard/main.py
@@ -1,26 +1,14 @@
 from dash import Dash
 import json
 
-with open("../Settings/settings.json") as settings_file
+import layout
+
+with open("../Settings/testsettings.json") as settings_file:
     settings = json.load(settings_file)
 DB_PATH = settings["Path to database"]
 
-if __name__ == '__main__':
-    try:
-        logs = SnowflakeLogs(PATH_TO_DB)
-    except Warning:
-        print("No connection established")
-    else:
-        # print(logs.figure_data_connections_at_day(datetime.today() - timedelta(days=20)))
-        print(logs.getDirectDbAccess().running_since())
-        connections = logs.get_connections_between(datetime(2022, 12, 30, 17, 00, 30),datetime(2022, 12, 30, 20, 00, 50))
-        print(connections)
-        print(connections["Connections"])
-
-# read DB path from config file?
-# make table variable?
-# connect db
-# build app
+app = Dash(__name__)
+app = layout.build_layout(app)
 
 
 #     app.run_server(debug=True)
