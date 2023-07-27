4c6f6191f2e721d9f61c6faaf2c8ba01882564e6
phklau <philipp.klausen@web.de>
2023-07-27 19:59:06 +0200

    main.py: Store example timebased sql querry as comment

--- Dashboard/main.py
+++ Dashboard/main.py
@@ -12,3 +12,13 @@ app = layout.build_layout(app)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
+#    try:
+#        logs = SnowflakeLogs(DB_PATH)
+#    except Warning:
+#       print("No connection established")
+#    else:
+#        # print(logs.figure_data_connections_at_day(datetime.today() - timedelta(days=20)))
+#        print(logs.getDirectDbAccess().running_since())
+#        connections = logs.get_connections_between(datetime(2022, 12, 30, 17, 00, 30),datetime(2022, 12, 30, 20, 00, 50))
+#        print(connections)
+#        print(connections["Connections"])
