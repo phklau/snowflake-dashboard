917a95e66cfa35e2fac52e6349848c1f50bf3143
phklau <philipp.klausen@web.de>
2023-07-28 18:36:54 +0200

    callbacks.py: Generate characteristic values according to picked range

--- Dashboard/callbacks.py
+++ Dashboard/callbacks.py
@@ -1,5 +1,13 @@
 from dash import Dash, callback, Input, Output, ctx
 from datetime import datetime, date, timedelta
+import json
+
+from SnowflakeLogs import SnowflakeLogs
+from format import DataSize
+
+with open("../Settings/testsettings.json") as settings_file:
+    settings = json.load(settings_file)
+DB_PATH = settings["Path to database"]
 
 
 def get_callbacks(app: Dash):
@@ -25,6 +33,32 @@ def get_callbacks(app: Dash):
             start_date,
             end_date
         )
-    # TODO: generate data according to picked data range
 
+    @app.callback(
+        Output('total-connections', 'children'),
+        Output('total-upload', 'children'),
+        Output('total-download', 'children'),
+        Output('total-errors', 'children'),
+        Input('date-picker', 'start_date'),
+        Input('date-picker', 'end_date'),
+    )
+    def update_metrics(start_date_iso, end_date_iso):
+        start_date = datetime.fromisoformat(start_date_iso)
+        end_date = datetime.fromisoformat(end_date_iso)
+
+        logs = SnowflakeLogs(DB_PATH)
+        logs.load_section_in_buffer(start_date, end_date)
+
+        total_connections = "{:,}".format(logs.get_total_connections())
+        total_upload = DataSize(logs.get_total_upload()).auto_format()
+        total_download = DataSize(logs.get_total_download()).auto_format()
+        total_errors = logs.get_total_errors()
+
+        del logs
+        return(
+            total_connections,
+            total_upload,
+            total_download,
+            total_errors,
+        )
     return app
