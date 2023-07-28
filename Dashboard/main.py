19302c64fd82fa8bf0b0490d231d79c948ec9456
phklau <philipp.klausen@web.de>
2023-07-28 11:32:56 +0200

    main.py: Expend app with callbacks

--- Dashboard/main.py
+++ Dashboard/main.py
@@ -2,6 +2,7 @@ from dash import Dash
 import json
 
 import layout
+import callbacks
 
 with open("../Settings/testsettings.json") as settings_file:
     settings = json.load(settings_file)
@@ -9,6 +10,7 @@ DB_PATH = settings["Path to database"]
 
 app = Dash(__name__)
 app = layout.build_layout(app)
+app = callbacks.get_callbacks(app)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
