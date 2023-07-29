27839a34ede603f05561313bb63abc41c267362b
phklau <philipp.klausen@web.de>
2023-07-29 15:28:31 +0200

    callbacks.py: Datepicker callback, empty buffer handling, connections and traffic graph

--- Dashboard/callbacks.py
+++ Dashboard/callbacks.py
@@ -1,9 +1,12 @@
 from dash import Dash, callback, Input, Output, ctx
 from datetime import datetime, date, timedelta
+import plotly.express as px
+from plotly import graph_objects
 import json
 
 from SnowflakeLogs import SnowflakeLogs
 from format import DataSize
+from GraphCreator import GraphCreator
 
 with open("../Settings/testsettings.json") as settings_file:
     settings = json.load(settings_file)
@@ -52,6 +55,8 @@ def get_callbacks(app: Dash):
         Output('total-upload', 'children'),
         Output('total-download', 'children'),
         Output('total-errors', 'children'),
+        Output('connections-graph', 'figure'),
+        Output('upload-download-graph', 'figure'),
         Input('date-picker', 'start_date'),
         Input('date-picker', 'end_date'),
     )
@@ -61,17 +66,32 @@ def get_callbacks(app: Dash):
 
         logs = SnowflakeLogs(DB_PATH)
         logs.load_section_in_buffer(start_date, end_date)
+        if logs.buffer_empty():
+            # TODO: Pop up/better error handling?
+            total_connections = "No Data available"
+            total_upload = "No Data available"
+            total_download = "No Data available"
+            total_errors = "No Data available"
+            connections_fig = px.line()
+            upload_download_fig = px.line()
+        else:
+            total_connections = "{:,}".format(logs.get_total_connections())
+            total_upload = DataSize(logs.get_total_upload()).auto_format()
+            total_download = DataSize(logs.get_total_download()).auto_format()
+            total_errors = logs.get_total_errors()
 
-        total_connections = "{:,}".format(logs.get_total_connections())
-        total_upload = DataSize(logs.get_total_upload()).auto_format()
-        total_download = DataSize(logs.get_total_download()).auto_format()
-        total_errors = logs.get_total_errors()
+            creator = GraphCreator(logs.get_buffered_logs())
+            connections_fig = creator.create_connections_graph()
+            upload_download_fig = creator.create_upload_download_graph()
 
         del logs
-        return(
+        return (
             total_connections,
             total_upload,
             total_download,
             total_errors,
+            connections_fig,
+            upload_download_fig,
         )
+
     return app
