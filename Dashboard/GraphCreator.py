c9f9242e293df3d163d4627a3d98974f5d50e8c0
phklau <philipp.klausen@web.de>
2023-07-29 18:29:53 +0200

    GraphCreator.py: Refactor seperate dataframes for traffic and error

--- Dashboard/GraphCreator.py
+++ Dashboard/GraphCreator.py
@@ -7,12 +7,14 @@ from datetime import datetime, date, timedelta
 class GraphCreator:
 
     def __init__(self, data: pd.DataFrame):
-        self.data = data
-        time_range = self.data.tail(1)["Timestamp"].item() - self.data.head(1)["Timestamp"].item()
-        self.data = data.set_index("Timestamp")
+        data = data.set_index("Timestamp")
+        data.sort_index(inplace=True)
+        self.traffic_data = data[["Connections", "Upload", "Download"]]
+        self.error_data = data[["Error", "Errortype", "Details"]]
+        time_range = self.traffic_data.tail(1).index.item() - self.traffic_data.head(1).index.item()
         if time_range > timedelta(weeks=2):
             intervall = timedelta(days=1)
-            self.data = self.data.resample(intervall).sum()
+            self.traffic_data = self.traffic_data.resample(intervall).sum()
 
     @staticmethod
     def __get_line_graph_prototype():
@@ -27,25 +29,27 @@ class GraphCreator:
         return graph
 
     def create_connections_graph(self):
+        connections_data = self.traffic_data
         graph = self.__get_line_graph_prototype()
-        max_value = self.data["Connections"].max()
+        max_value = self.traffic_data["Connections"].max()
         graph.update_layout(yaxis_range=[0, max_value * 1.1], height=150)
-        trace = graph_objects.Scatter(x=self.data.index, y=self.data["Connections"], line=dict(color="dodgerblue"))
+        trace = graph_objects.Scatter(x=connections_data.index, y=connections_data["Connections"],
+                                      line={'color': "dodgerblue"})
         graph.add_trace(trace)
         return graph
 
     def create_upload_download_graph(self):
         upload_key = "Upload"
         download_key = "Download"
-        download_data_inverted = self.data[download_key].multiply(-1)
+        download_data_inverted = self.traffic_data[download_key].multiply(-1)
         graph = self.__get_line_graph_prototype()
-        max_upload = self.data[upload_key].max()
+        max_upload = self.traffic_data[upload_key].max()
         max_download = download_data_inverted.min()
         graph.update_layout(yaxis_range=[max_download * 1.1, max_upload * 1.1],
                             height=150,
                             yaxis={'ticksuffix': " MB"})
-        upload_trace = graph_objects.Scatter(x=self.data.index, y=self.data[upload_key], name="Upload", fill='tozeroy')
-        download_trace = graph_objects.Scatter(x=self.data.index, y=download_data_inverted, name="Download", fill='tozeroy')
+        upload_trace = graph_objects.Scatter(x=self.traffic_data.index, y=self.traffic_data[upload_key], name="Upload", fill='tozeroy')
+        download_trace = graph_objects.Scatter(x=self.traffic_data.index, y=download_data_inverted, name="Download", fill='tozeroy')
         graph.add_trace(download_trace)
         graph.add_trace(upload_trace)
         return graph
