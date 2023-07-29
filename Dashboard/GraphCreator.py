6cbf1c935dd8b98c1c87363565c18146505fca57
phklau <philipp.klausen@web.de>
2023-07-29 13:28:48 +0200

    GraphCreator.py: Prototype for connection and down/upload linegraphs

--- Dashboard/GraphCreator.py
+++ Dashboard/GraphCreator.py
@@ -1,3 +1,5 @@
+import plotly.express as px
+from plotly import graph_objects
 import pandas as pd
 from datetime import datetime, date, timedelta
 
@@ -11,4 +13,16 @@ class GraphCreator:
         if time_range > timedelta(weeks=2):
             intervall = timedelta(days=1)
             self.data = self.data.resample(intervall).sum()
+
+    @staticmethod
+    def __get_line_graph_prototype():
+        graph = px.line()
+        graph.update_layout(
+            plot_bgcolor='rgb(255,200,255)',
+            paper_bgcolor='rgb(255,255,255)',
+            xaxis_title=None,
+            yaxis_title=None,
+            margin={'l': 10, 'r': 10, 't': 10, 'b': 10},
+        )
+        return graph
 
