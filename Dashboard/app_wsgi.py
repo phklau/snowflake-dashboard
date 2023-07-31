5a03cc732dcb6b501d265d1b70298b34fc03bcfc
phklau <philipp.klausen@web.de>
2023-07-31 13:39:12 +0200

    Fix wsgi imports

--- Dashboard/app_wsgi.py
+++ Dashboard/app_wsgi.py
@@ -1,20 +1,19 @@
 import sys
-import json
 from pathlib import Path
+
 from dash import Dash
+
+APP_PATH = str(Path(__file__).parent)
+if APP_PATH not in sys.path:
+    sys.path.append(APP_PATH)
 
 from App import layout, callbacks
 
-app = Dash(__name__)
+app = Dash(__name__, requests_pathname_prefix='/dashboard/')
 app = layout.build_layout(app)
 app = callbacks.get_callbacks(app)
 app.css.config.serve_locally = True
+app.scripts.config.serve_locally = True
 
-settings_path = Path(__file__).parent.parent.joinpath("Settings/dashboard.json")
-with open(settings_path) as settings_file:
-    settings = json.load(settings_file)
-APP_PATH = settings["Path to app"]
-if APP_PATH not in sys.path:
-    sys.path = [APP_PATH] + sys.path
 
 application = app.server
