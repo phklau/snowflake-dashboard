f63d51d409221e6cdd62f3d6122bbd4b4cfa304b
phklau <philipp.klausen@web.de>
2023-08-07 22:15:43 +0200

    Dir struct for settings

---
+++ Installation/templates/InstallationSettings.bash
@@ -0,0 +1,6 @@
+# Path to the database and logfiles where the logs are stored
+# Use absolute path or relative to the root of the repository
+DB_PATH="./"
+LOGFILE_PATH="./"
+# Path from where the webserver will serve the dashboard
+WEBAPP_PATH="/var/www/snowflake-dashboard/"
