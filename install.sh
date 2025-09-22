69aeac59a6e82ed59017fc0514eed27135d912a2
phklau <107357845+phklau@users.noreply.github.com>
2025-09-22 20:45:02 +0200

    Support new logging format of Snowflake 2.8.0 (#15)
    * LogParser: refactor LogParser class to use abstract class, polymorphism for different versions
    
    * logToDb.py: Handle debugging without settings file
    
    * LogParser: Implement new class parsing logs since changes in 2.8.0
    
    * logToDb.py: Instanciate right LogParser depending on version number
    
    * InstallationSettings.bash: Enhance variable description
    
    * install.sh&logger.json: Generate SnowflakeVersion variable during installation
    
    * Use venv for logger
    
    * install.sh: Current working snowflake version
    
    * install.sh: Refactor version regex usage
    
    * install.sh: snowflake version inital version number to enable updatability

--- install.sh
+++ install.sh
@@ -3,6 +3,9 @@
 # set -x
 set -e
 
+SUPPORTED_SNOWFLAKE_VERSION="2.9.2"
+VERSION_REGEX='[0-9]+\.[0-9]+\.?[0-9]+'
+
 
 usage () {
     cat <<EOF
@@ -45,8 +48,8 @@ load_settings () {
             WEBAPP_PATH=${WEBAPP_PATH%/}
             SNOWFLAKE_PATH=${SNOWFLAKE_PATH%/}
 
-            if [[ $SNOWFLAKE_PATH == "" || $SNOWFLAKE_USER == "" || $SNOWFLAKE_GROUP == "" || SERVER_NAME == "" ]]; then
-                echo "No snowflake settings found"
+            if [[ $SNOWFLAKE_PATH == "" || $SNOWFLAKE_USER == "" || $SNOWFLAKE_GROUP == "" || $SERVER_NAME == "" ]]; then
+                echo "Some of the snowflake settings variables might be empty"
                 echo "Please check again InstallationSettings.bash"
                 echo "Aborting ..."
                 exit
@@ -59,6 +62,18 @@ create_settings () {
     cp ./Installation/templates/*.json ./Settings/
     chown -R ${SNOWFLAKE_USER}:${SNOWFLAKE_GROUP} ./Settings/
     sed -i "s/PWD/${PWD//\//\\/}/g" ./Settings/dashboard.json
+    set_snowflake_version
+}
+
+# needs load_settings() run
+set_snowflake_version () {
+    SNOWFLAKE_VERSION=$($SNOWFLAKE_PATH/proxy/proxy --version 2>&1 | grep -o -E $VERSION_REGEX)
+    echo "Snowflake version $SNOWFLAKE_VERSION detected"
+    if dpkg --compare-versions $SNOWFLAKE_VERSION gt $SUPPORTED_SNOWFLAKE_VERSION; then
+        echo "WARNING!: snowflake version is not supported and might cause problems parsing the logs"
+        echo "Check the database if logs for errors of the type <Parser>"
+    fi
+    sed -i s/"[0-9]\+\.[0-9]\+\.[0-9]\+"/${SNOWFLAKE_VERSION}/g ./Settings/logger.json
 }
 
 create_data_storage () {
@@ -76,7 +91,7 @@ install_packages () {
 
 check_python_version () {
     SUPPORTED_VERSIONS=("3.11.2" "3.13.5")
-    VERSION=$(python3 --version | grep -o -E '[0-9]+\.[0-9]+\.?[0-9]+')
+    VERSION=$(python3 --version | grep -o -E $VERSION_REGEX)
     SUPPORTED=false
 
     for value in "${SUPPORTED_VERSIONS[@]}"
@@ -183,8 +198,9 @@ main () {
         case $1 in
             -u | --update-all)
                 echo "Updating snowflake with logging service ..."
-                systemctl restart snowflake-with-logger.service
                 load_settings
+                set_snowflake_version
+                systemctl restart snowflake-with-logger.service
                 update_web_app
                 ;;
             --update-dashboard)
@@ -194,13 +210,13 @@ main () {
             -i | --install)
                 preinstall_check
                 install_packages
-                # Logger
                 create_settings
-                create_data_storage
-                create_systemd_service
                 # Dashboard
                 create_pyenv
                 install_apache
+                # Logger
+                create_data_storage
+                create_systemd_service
                 ;;
 
             --install-service)
